e7e23a56641b337944860e421a7bc5880a8e25ea
gkampitakis <gkabitakis@gmail.com>
2020-12-24 10:38:34 +0000

    Remove router and about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,18 @@
-import React, { lazy, Suspense } from 'react';
-import Home from './Pages/Home';
+import React from 'react';
+import Home from './Home';
 import Navbar from './components/Navbar/Navbar';
 import { AppProvider } from './context';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
 import './app.scss';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const About = lazy(() => import('./Pages/About'));
 
 function App () {
   return (
     <main className="app">
       <AppProvider>
-        <Router>
-          <ToastContainer />
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route path='/about' render={() =>
-              <Suspense fallback={'Loading ...'}><About /></Suspense >}
-            />
-          </Switch>
-        </Router>
+        <ToastContainer />
+        <Navbar />
+        <Home />
       </AppProvider>
     </main>
   );
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ControlPanel from '../components/ControlPanel/ControlPanel';
-import ImagePreviewer from '../components/Preview/ImagePreviewer';
+import ControlPanel from './components/ControlPanel/ControlPanel';
+import ImagePreviewer from './components/Preview/ImagePreviewer';
 
 export default function Home () {
   return (
